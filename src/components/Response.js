c7deda75cb68f06e2636c00c817a2d133a4928bc
zzztor <snake_raiden35@hotmail.com>
2020-09-05 20:12:06 +0200

    Feature: Text to speech

--- src/components/Response.js
+++ src/components/Response.js
@@ -1,18 +1,37 @@
 import React from 'react';
 
 class Response extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            speechSynthesis: 'speechSynthesis' in window ? new SpeechSynthesisUtterance() : undefined
+        }
+
+    }
 
     translateText(text) {
         const translatedText = text.replace(/[aeou]/g, 'i').replace(/[áéóú]/g, 'í').replace(/[àèòù]/g, 'ì').replace(/[AEOU]/g, 'I').replace(/qii/g, 'qui').replace(/QII/g, 'QUI').replace(/qií/g, 'quí').replace(/QIÍ/g, 'QUÍ');
 
         return translatedText;
     }
+    playTextToSpeech (text) {
+        this.state.speechSynthesis.text = this.translateText(text);
+        this.state.speechSynthesis.lang = 'es';
+        window.speechSynthesis.speak(this.state.speechSynthesis);
+    }
 
     render() {
         const text = this.props.textToTranslate;
+        let button;
+        if (this.state.speechSynthesis && text) {
+            button = <button className="button" title="Texto a voz" onClick={this.playTextToSpeech.bind(this, text)}> 
+                <i className='far fa-play-circle'></i> 
+            </button>
+        }
         return ( 
         <div className="response">
-            <p > { this.translateText(text) } </p> 
+            <p> { this.translateText(text) }</p>
+            {button}
             </div >
         );
     }
